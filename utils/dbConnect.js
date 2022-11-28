214e1c0f24b6d1a66c89afb53260d29e629785f3
Sbipul <bipulcroy121@gmail.com>
2022-11-28 21:15:43 +0600

    complete node-mongo module3

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,4 @@
 const dbConnect = ()=>{
-    // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ew1rb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('database connected')
 }
 module.exports = dbConnect;
