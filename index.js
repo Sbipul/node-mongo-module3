9c49e5fda3360aaea2c37dec7d36832467f4318a
Sbipul <bipulcroy121@gmail.com>
2022-11-28 20:46:10 +0600

    view engine set up

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const productsRoute = require("./routes/v1/products.route");
 // middleware calls 
 app.use(cors())
 app.use(express.json())
-// app.use(viewCount)
+app.use(express.static("public"))
+app.set('view engine','ejs')
+
 // database connect 
 dbConnect()
 
@@ -34,7 +36,10 @@ const run = async() =>{
 run().catch(console.dir)
 
 app.get('/',(req,res)=>{
-    res.send('This is home')
+    // res.sendFile(__dirname + "/public/index.html")
+    res.render("home.ejs",{
+        id:5
+    })
 })
 app.all("*",(req,res)=>{
     res.send("No route found")
